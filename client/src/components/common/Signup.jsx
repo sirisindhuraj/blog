e8ec4b2f083abd87ffe445fbe912d7ee8d356d5c
Siri Sindhu Raj <sirisindhuraj@Siris-MacBook-Air.local>
2025-03-21 22:42:59 +0530

    added blog changes

---
+++ client/src/components/common/Signup.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {SignUp} from '@clerk/clerk-react'
+
+function SIgnup() {
+  return (
+    <div className='d-flex justify-content-center align-items-center h-100'>
+      <SignUp />
+    </div>
+  )
+}
+
+export default SIgnup
